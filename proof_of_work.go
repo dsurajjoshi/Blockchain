73e995df4ceb7b41e78f8666b0fb54c08fb92ab0
Suraj Joshi <joshisuraj.151@gmail.com>
2022-11-12 14:47:59 +0530

    Update proof_of_work.go

--- proof_of_work.go
+++ proof_of_work.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 	"time"
@@ -26,6 +27,39 @@ func hashGenerator(data, timestamp, nounce, difficulty string) string {
 	return created_hash
 }
 
+func decimalToBinary(data []int) []int {
+	binary_value := []int{}
+	for _, ascii := range data {
+		remainder_array := []int{}
+		for ascii >= 0 {
+			numerator, remainder := ascii/2, ascii%2
+			remainder_array = append(remainder_array, remainder)
+			if ascii == 0 {
+				break
+			}
+			ascii = numerator
+		}
+		for i := len(remainder_array) - 1; i >= 0; i-- {
+			binary_value = append(binary_value, remainder_array[i])
+		}
+	}
+	return binary_value
+}
+
+func hexToBinary(data string) []int {
+	decimal := 0
+	hex_numbers := make(map[string]int)
+	length_of_hex := len(data) - 1
+	hex_numbers["0"], hex_numbers["1"], hex_numbers["2"], hex_numbers["3"], hex_numbers["4"], hex_numbers["5"], hex_numbers["6"], hex_numbers["7"], hex_numbers["8"], hex_numbers["9"], hex_numbers["a"], hex_numbers["b"], hex_numbers["c"], hex_numbers["d"], hex_numbers["e"], hex_numbers["f"] = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+	for i, chrac_bit := range data {
+		hexmultiplier := math.Pow(16, float64(length_of_hex-i))
+		decimal += hex_numbers[string(chrac_bit)] * int(hexmultiplier)
+	}
+	decimal_value := []int{decimal}
+	binary := decimalToBinary(decimal_value)
+	return binary
+}
+
 func main() {
 	fmt.Println("My First Blockchain Program")
 	block_data := []string{"My Name is Suraj Joshi", "Learning BloakChain", "With Golang"}
@@ -42,7 +76,8 @@ func main() {
 		prevHash := blocks.Back()
 		prevHash_value := addBlock(prevHash.Value.(addBlock))
 		new_hash := string(hashGenerator(block_data[i], time, strconv.Itoa(nounce), strconv.Itoa(difficulty)))
-		for strings.Index(new_hash, strings.Repeat("0", difficulty)) != 0 {
+		new_hash_binary := hexToBinary(new_hash)
+		for strings.Index(strconv.Itoa(new_hash_binary[0]), strings.Repeat("0", difficulty)) != 0 {
 			nounce += 1
 			new_hash = string(hashGenerator(block_data[i], time, strconv.Itoa(nounce), strconv.Itoa(difficulty)))
 			fmt.Println("count: 3: ", new_hash)
